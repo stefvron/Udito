3478d4c4deb7cac83fafdfb9771f5316dad55c23
Stefvron <stefvron@steffenmolitor.de>
2025-02-05 04:24:51 +0100

    Added Atkinson dithering

--- main.js
+++ main.js
@@ -69,6 +69,7 @@ const palLengths = {
 const algorithms = {
     "nc": nearestColour,
     "fs": floydSteinberg,
+    "atk": atkinson,
     "bm4": bayerMatrix4,
     "bm8": bayerMatrix8,
 }
@@ -476,7 +477,38 @@ function floydSteinberg(pal) {
             const offsets = [[1,0],[-1,1],[0,1],[1,1]];
             const fact = [(7/16),(3/16),(5/16),(1/16)];
             for(let o = 0; o < offsets.length; o++) {
-                const c = (y+offsets[o][1]) * (imageData.width * 4) + (x+offsets[o][0])  * 4;
+                const c = (y+offsets[o][1]) * (imageData.width * 4) + (x+offsets[o][0]) * 4;
+                data[c] += (quantError[0] * fact[o] * errfacIn.value); // red
+                data[c + 1] += (quantError[1] * fact[o] * errfacIn.value); // green
+                data[c + 2] += (quantError[2] * fact[o] * errfacIn.value); // blue
+            }
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
+
+function atkinson(pal) {
+    const ctx = previewImg.getContext("2d");
+    const Sctx = sourceImg.getContext("2d");
+    const imageData = Sctx.getImageData(0, 0, previewImg.width, previewImg.height);
+    const data = imageData.data;
+    for(let y = 0; y < imageData.height; y++) {
+        for(let x = 0; x < imageData.width; x++) {
+            const i = y * (imageData.width * 4) + x * 4;
+            const oldpixel = [data[i],data[i+1],data[i+2]]
+            newpixel = getNearestColourFromPaletteSync([...oldpixel], pal);
+
+            data[i] = newpixel[0]; // red
+            data[i + 1] = newpixel[1]; // green
+            data[i + 2] = newpixel[2]; // blue
+
+            const quantError = [(oldpixel[0] - newpixel[0]), (oldpixel[1] - newpixel[1]), (oldpixel[2] - newpixel[2])];
+            
+            const offsets = [[1,0],[2,0],[-1,1],[0,1],[1,1],[0,2]];
+            const fact = [(1/8),(1/8),(1/8),(1/8),(1/8),(1/8)];
+            for(let o = 0; o < offsets.length; o++) {
+                const c = (y+offsets[o][1]) * (imageData.width * 4) + (x+offsets[o][0]) * 4;
                 data[c] += (quantError[0] * fact[o] * errfacIn.value); // red
                 data[c + 1] += (quantError[1] * fact[o] * errfacIn.value); // green
                 data[c + 2] += (quantError[2] * fact[o] * errfacIn.value); // blue
