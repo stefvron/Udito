d53bd901297448ba3e2f26700e8994ec842d9705
stefvron <89789348+stefvron@users.noreply.github.com>
2026-01-14 01:17:08 +0100

    Fixed CIE76

--- scripts/module/colour/cie76.js
+++ scripts/module/colour/cie76.js
@@ -1,7 +1,11 @@
-import './common.js';
+import { rgbToLab } from './common.js';
 
 export default function cDist(c1, c2) {
     let lab1 = rgbToLab(c1);
     let lab2 = rgbToLab(c2);
-    return dist(lab1, lab2);
+    return Math.sqrt(
+        Math.pow(lab2[0]-lab1[0],2) +
+        Math.pow(lab2[1]-lab1[1],2) +
+        Math.pow(lab2[2]-lab1[2],2)
+    );
 }
